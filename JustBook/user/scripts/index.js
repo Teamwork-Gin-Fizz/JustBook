c889687dabd1b21d67fe1f507372f3f99118c4e1
dimitarsd <dsd363978@yahoo.com>
2015-08-25 11:25:41 +0300

    Added requireJS to index.html and created start point for the main app in index.js with requireJS

---
+++ JustBook/user/scripts/index.js
@@ -0,0 +1,15 @@
+(function () {
+	'use strict';
+	
+	require.config({
+		path:{
+			'jQuery': 'libs/jquery',
+			'Sammy': 'libs/sammy'
+		}
+	});
+	
+	// Starting the app - to be implemented
+	require([], function(){
+		
+	});
+} ());
