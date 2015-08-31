355cfc645c245473a6ae91fcf86bbb2552f8d2e8
Pavel <pawel.angelow@gmail.com>
2015-08-31 12:40:37 +0300

    Added basic user experience for register and login

--- JustBook/user/scripts/register.js
+++ JustBook/user/scripts/register.js
@@ -2,7 +2,7 @@ define(['jquery'], function ($) {
 
     var $template = $('#template'),
         serverIP = 'http://78.90.130.123/server.php';
-        
+
     $template.on('click', '#register-button', registerLogic);
 
     function registerLogic() {
@@ -15,23 +15,36 @@ define(['jquery'], function ($) {
             
         // TODO: If nevalid data -> display errors    
         if (chosenUsername != '' && chosenPassword != '') {
-            $.getJSON(serverIP + '?callback=?', 
-                'action=' + 'register'+ 
-                '&name='+ chosenUsername + 
+            $.getJSON(serverIP + '?callback=?',
+                'action=' + 'register' +
+                '&name=' + chosenUsername +
                 '&password=' + chosenPassword +
                 '&firstname=' + firstName +
                 '&lastname=' + lastName +
-                '&gender=' + gender + 
-                '&birthdate=' + birthDate, 
+                '&gender=' + gender +
+                '&birthdate=' + birthDate,
                 function (res) {
-                /*
-                Server returns:
-                Object { answer: "incorrect", reason: "username" } //when username is not in DB
-                Object { answer: "incorrect", reason: "password" } //when password is not that one from DB
-                Object { answer: "correct", hash: "12345678" } //when everything is OK
-                */
-                console.log(res);
-            });
+                    if (res.answer == 'success') {
+                        requirejs(['../inner-template-loader'], function (templateLoader) {
+                            templateLoader.loadTemplate('#inner-window', 'templates/signup-page-result-success.html');
+                        });
+                    } else {
+                        requirejs(['../inner-template-loader'], function (templateLoader) {
+                            templateLoader.loadTemplate('#inner-window', 'templates/signup-page-result-fail.html');
+                        });
+                    }
+                    clearForm();
+                    console.log(res);
+                });
+        }
+
+        function clearForm() {
+            firstName = $('#sign-up-firstname').val(''),
+            lastName = $('#sign-up-lastname').val(''),
+            birthDate = $('#sign-up-birthdate').val(''),
+            gender = $('#sign-up-gender').val(''),
+            chosenUsername = $('#sign-up-username').val(''),
+            chosenPassword = $('#sign-up-password').val('');
         }
     }
 });
