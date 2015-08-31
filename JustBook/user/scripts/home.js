f1b99f63c9cc3fca67fc458d49915ce16013874e
Pavel <pawel.angelow@gmail.com>
2015-08-31 15:00:01 +0300

    Added chat functionality

--- JustBook/user/scripts/home.js
+++ JustBook/user/scripts/home.js
@@ -6,10 +6,20 @@ define(['jquery'], function ($) {
 	$('#username').html(sessionStorage.getItem('username'));
 
 	$template.on('click', '#go-to-chat', ChatLoadLogic);
+	$template.on('click', '#go-to-logout', Logout);
 
 	function ChatLoadLogic() {
 		requirejs(['../inner-template-loader'], function (templateLoader) {
 			templateLoader.loadTemplate('#inner-content', 'templates/chat-main-page.html');
 		});
+		requirejs(['../chat-logic']);
+	};
+	
+	function Logout(){
+		sessionStorage.setItem('username', '');
+		sessionStorage.setItem('userHash', '');
+		requirejs(['../template-loader'], function (templateLoader) {
+            templateLoader.loadCustonPage('templates/main-page-template.html');
+        });
 	};
 });
