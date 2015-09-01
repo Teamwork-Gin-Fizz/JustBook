a300402fd7094d8c236b1445046b9f5db5d64e8b
SimoPrG <simeon_petev@hotmail.com>
2015-09-01 14:26:09 +0300

    Added routing between #/ #/sign-in and #/sign-up

--- JustBook/user/scripts/index.js
+++ JustBook/user/scripts/index.js
@@ -5,15 +5,41 @@
 		baseUrl: 'scripts/libs',
 		path:{
 			'jquery': 'jquery',
-			'Sammy': 'sammy',
+			'sammy': 'sammy',
 			'bootstrap':'bootstrap'
 		}
 	});
-	
-	// Starting the app - to be implemented
-	requirejs(['../template-loader'], function(templateLoader){
-		templateLoader.loadMainPage();
+
+	requirejs(['jquery', 'sammy', '../inner-template-loader'], function ($, sammy, templateLoader) {
+	    var app = sammy('#template', function () {
+	        this.get('#/', function () {
+	            templateLoader.loadTemplate('#template', 'templates/main-page-template.html');
+	        });
+
+			this.get('#/sign-in', function () {
+				templateLoader.loadTemplate('#template', 'templates/main-page-signin-template.html');
+			});
+
+			this.get('#/sign-up', function () {
+				templateLoader.loadTemplate('#template', 'templates/main-page-signup-template.html');
+			});
+
+			this.get('#/home', function () {
+			    
+			});
+
+			this.get('#/home/chat', function () {
+
+			});
+	    });
+
+		app.run('#/');
 	});
+
+	// Starting the app - to be implemented
+	//requirejs(['../template-loader'], function(templateLoader){
+	//	templateLoader.loadMainPage();
+	//});
 	
 	requirejs(['../login']);
 	
